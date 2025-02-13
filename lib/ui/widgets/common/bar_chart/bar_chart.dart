41c0f86637b616301cfb57bcf9f7dacdbe4cd9cb
ReaganBuster <reaganssebbaale@gmail.com>
2025-02-13 22:33:29 +0300

    bar chart update

--- lib/ui/widgets/common/bar_chart/bar_chart.dart
+++ lib/ui/widgets/common/bar_chart/bar_chart.dart
@@ -19,34 +19,55 @@ class BarChart extends StackedView<BarChartModel> {
       child: SizedBox(
         width: screenWidth(context) * 0.57,
         height: screenHeight(context) / 3,
-        child: charts.BarChart(
-          _createSampleData(),
-          animate: true,
+        child: Column(
+          children: [
+            const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Order Accuracy',
+                    style: TextStyle(),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: screenWidth(context) * 0.5,
+              height: screenHeight(context) / 4,
+              child: charts.BarChart(
+                _createSampleData(),
+                animate: true,
 
-          /// Assign a custom style for the measure axis.
-          ///
-          /// The NoneRenderSpec only draws an axis line (and even that can be hidden
-          /// with showAxisLine=false).
+                /// Assign a custom style for the measure axis.
+                ///
+                /// The NoneRenderSpec only draws an axis line (and even that can be hidden
+                /// with showAxisLine=false).
 
-          primaryMeasureAxis:
-              const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
+                primaryMeasureAxis: const charts.NumericAxisSpec(
+                    // showAxisLine: true, renderSpec: charts.NoneRenderSpec()
+                    ),
 
-          /// This is an OrdinalAxisSpec to match up with BarChart's default
-          /// ordinal domain axis (use NumericAxisSpec or DateTimeAxisSpec for
-          /// other charts).
-          domainAxis: const charts.OrdinalAxisSpec(
-              // Make sure that we draw the domain axis line.
-              showAxisLine: true,
-              // But don't draw anything else.
-              renderSpec: charts.NoneRenderSpec()),
+                /// This is an OrdinalAxisSpec to match up with BarChart's default
+                /// ordinal domain axis (use NumericAxisSpec or DateTimeAxisSpec for
+                /// other charts).
+                domainAxis: const charts.OrdinalAxisSpec(
+                  // Make sure that we draw the domain axis line.
+                  showAxisLine: false,
+                  // But don't draw anything else.
+                  // renderSpec: charts.NoneRenderSpec()
+                ),
 
-          // With a spark chart we likely don't want large chart margins.
-          // 1px is the smallest we can make each margin.
-          layoutConfig: charts.LayoutConfig(
-              leftMarginSpec: charts.MarginSpec.fixedPixel(0),
-              topMarginSpec: charts.MarginSpec.fixedPixel(0),
-              rightMarginSpec: charts.MarginSpec.fixedPixel(0),
-              bottomMarginSpec: charts.MarginSpec.fixedPixel(0)),
+                // With a spark chart we likely don't want large chart margins.
+                // 1px is the smallest we can make each margin.
+                layoutConfig: charts.LayoutConfig(
+                    leftMarginSpec: charts.MarginSpec.fixedPixel(0),
+                    topMarginSpec: charts.MarginSpec.fixedPixel(0),
+                    rightMarginSpec: charts.MarginSpec.fixedPixel(0),
+                    bottomMarginSpec: charts.MarginSpec.fixedPixel(0)),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -55,23 +76,24 @@ class BarChart extends StackedView<BarChartModel> {
   /// Create series list with single series
   static List<charts.Series<OrdinalSales, String>> _createSampleData() {
     final globalSalesData = [
-      OrdinalSales('Jan', 3100),
-      OrdinalSales('Feb', 3500),
-      OrdinalSales('Mar', 5000),
-      OrdinalSales('Apr', 2500),
-      OrdinalSales('May', 3200),
-      OrdinalSales('June', 4500),
-      OrdinalSales('July', 4400),
-      OrdinalSales('Aug', 5000),
-      OrdinalSales('Sep', 5000),
-      OrdinalSales('Oct', 4500),
-      OrdinalSales('Nov', 4300),
-      OrdinalSales('Dec', 4600),
+      OrdinalSales('Jan', 30),
+      OrdinalSales('Feb', 40),
+      OrdinalSales('Mar', 36),
+      OrdinalSales('Apr', 30),
+      OrdinalSales('May', 70),
+      OrdinalSales('June', 39),
+      OrdinalSales('July', 30),
+      OrdinalSales('Aug', 70),
+      OrdinalSales('Sep', 80),
+      OrdinalSales('Oct', 60),
+      OrdinalSales('Nov', 80),
+      OrdinalSales('Dec', 40),
     ];
 
     return [
       charts.Series<OrdinalSales, String>(
         id: 'Global Revenue',
+        colorFn: (_, __) => charts.MaterialPalette.gray.shadeDefault,
         domainFn: (OrdinalSales sales, _) => sales.month,
         measureFn: (OrdinalSales sales, _) => sales.sales,
         data: globalSalesData,
