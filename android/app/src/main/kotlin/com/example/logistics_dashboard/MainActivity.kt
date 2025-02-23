55ce1880c1b35d18ed80a0cb92d0867c2f7c405d
ReaganBuster <reaganssebbaale@gmail.com>
2025-02-23 10:14:47 +0300

    dashboard update

--- android/app/src/main/kotlin/com/example/logistics_dashboard/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.logistics_dashboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/ui/widgets/common/pie_chart/pie_chart_model.dart
+++
@@ -1,3 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class PieChartModel extends BaseViewModel {}
